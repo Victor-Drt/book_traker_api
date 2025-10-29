11e4d7af70548273c2b275504eaa1b016341871c
Victor-Drt <victorduarte778@gmail.com>
2025-10-29 15:00:52 -0400

    feat(users): adicionar serializaçao e viewset de criação de usuario

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ViewSet):
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
