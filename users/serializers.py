e7ebb8c6ec4789ee8849904ba111f60be0b44663
Victor-Drt <victorduarte778@gmail.com>
2025-10-30 11:00:12 -0400

    feat(users): adicionar restrição write only na visualização de password

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     
+    password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'date_joined']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # hash da senha
+        user.save()
+        return user
