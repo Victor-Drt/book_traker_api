8adcabe77c5d3d2b4668341fa8006efe3656cf51
Victor-Drt <victorduarte778@gmail.com>
2025-11-03 10:53:05 -0400

    feat(progress): adicionar model de Progress

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from books.models import Books
+
 
 # Create your models here.
+class Progress(models.Model):
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    pages_read = models.IntegerField(default=0)
