76d510a08c48b2248129c67b0cd25665d2905719
Victor-Drt <victorduarte778@gmail.com>
2025-11-10 14:49:42 -0400

    feat(books): adicionar view de estatisticas

--- books/urls.py
+++ books/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import BookViewSet
+from .views import BookViewSet, StatsViewSet
 
 router = routers.DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
+router.register(r'stats', StatsViewSet, basename='stats')
 
 urlpatterns = [
     path('', include(router.urls)),
--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Sum
+from django.db.models.functions import TruncWeek, TruncMonth
 
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -29,10 +31,52 @@ class BookViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         progress = {
-            "avg_pages_by_day": book.total_pages_read,
+            "avg_pages_by_day": book.total_pages_read, # ajustar
             "percent_finished": book.percent_finished
         }
 
         # progress = Progress.objects.filter(book=book.id)
         # serializer = ProgressSerializer(progress, many=True)
         return Response(progress, status=status.HTTP_200_OK)
+
+
+class StatsViewSet(viewsets.ViewSet):
+    def list(self, request):
+        user = request.user
+
+        books = Books.objects.filter(owner=user, is_finished=True)
+        readed_books = books.count()
+
+        progresses = Progress.objects.filter(book__owner=user)
+
+        weekly_data = (
+            progresses.annotate(week=TruncWeek("date"))
+            .values("week")
+            .annotate(total_pages=Sum("pages_read"))
+            .order_by("week")
+        )
+
+        pages_by_week = {
+            str(entry["week"].date()): entry["total_pages"]
+            for entry in weekly_data
+        }
+
+        monthly_data = (
+            progresses.annotate(month=TruncMonth("date"))
+            .values("month")
+            .annotate(total_pages=Sum("pages_read"))
+            .order_by("month")
+        )
+
+        pages_by_month = {
+            entry["month"].strftime("%Y-%m"): entry["total_pages"]
+            for entry in monthly_data
+        }
+
+        data = {
+            "books_read": readed_books,
+            "pages_by_week": pages_by_week,
+            "pages_by_month": pages_by_month,
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
