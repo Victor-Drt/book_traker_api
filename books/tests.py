a8c95dacb29704e6edd22cdfbafd3441b34c86df
Victor-Drt <victorduarte778@gmail.com>
2025-11-11 15:02:03 -0400

    test: adicionar primeiros testes de exemplo

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from pytest_drf import APIViewTest, AsUser, Returns200, UsesGetMethod
+from pytest_lambda import lambda_fixture
 
-# Create your tests here.
+alice = lambda_fixture(
+    lambda: User.objects.create(
+        username='alice',
+        first_name='Alice',
+        last_name='Innchains',
+        email='alice@ali.ce',
+    ))
+
+class TestAboutMe(
+    APIViewTest,
+    UsesGetMethod,
+    Returns200,
+    AsUser('alice'),
+):
+    url = lambda_fixture(lambda: reverse('about-me'))
+
+    def test_it_returns_profile(self, json):
+        expected = {
+            'username': 'alice',
+            'first_name': 'Alice',
+            'last_name': 'Innchains',
+            'email': 'alice@ali.ce',
+        }
+        actual = json
+        assert expected == actual
