6765bea92460efc082ab32d06deb566d9067bc9a
Victor-Drt <victorduarte778@gmail.com>
2025-11-12 15:00:09 -0400

    test: adicionar testes de crud da viewset de books

--- books/tests.py
+++ books/tests.py
@@ -1,30 +1,41 @@
 from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
 from django.urls import reverse
-from pytest_drf import APIViewTest, AsUser, Returns200, UsesGetMethod
-from pytest_lambda import lambda_fixture
+from rest_framework import status
+from books.models import Books
 
-alice = lambda_fixture(
-    lambda: User.objects.create(
-        username='alice',
-        first_name='Alice',
-        last_name='Innchains',
-        email='alice@ali.ce',
-    ))
 
-class TestAboutMe(
-    APIViewTest,
-    UsesGetMethod,
-    Returns200,
-    AsUser('alice'),
-):
-    url = lambda_fixture(lambda: reverse('about-me'))
+class BookViewSetTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="victor", password="123456")
+        self.client.force_authenticate(user=self.user)
 
-    def test_it_returns_profile(self, json):
-        expected = {
-            'username': 'alice',
-            'first_name': 'Alice',
-            'last_name': 'Innchains',
-            'email': 'alice@ali.ce',
+        self.url_list = reverse("books-list")
+        self.url_detail = lambda pk: reverse("books-detail", args=[pk])
+
+        self.book = Books.objects.create(
+            title="Django 101",
+            author="Novo Autor",
+            category="Categoria",
+            total_pages=100,
+            owner=self.user,
+        )
+
+    def test_list_books(self):
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_book(self):
+        response = self.client.get(self.url_detail(self.book.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_book(self):
+        data = {
+            "title": "Novo Livro",
+            "author": "Autor Teste",
+            "category": "Categoria X",
+            "total_pages": 150,
+            "owner": self.user.id
         }
-        actual = json
-        assert expected == actual
+        response = self.client.post(self.url_list, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
