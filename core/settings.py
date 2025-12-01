32bf69703c70f19565347ba9e9137ac60871a819
Victor-Drt <victorduarte778@gmail.com>
2025-12-01 09:08:58 -0400

    feat: configurar PostgreSQL via DATABASE_URL com connection pooling

--- core/settings.py
+++ core/settings.py
@@ -125,11 +125,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
