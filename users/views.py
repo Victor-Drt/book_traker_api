755fe1d1a90e63fb17017e4a99f4c9c9fbb050d1
Victor-Drt <victorduarte778@gmail.com>
2025-12-01 11:23:05 -0400

    feat: adicionar permissao para cadastro de usuarios

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
@@ -9,6 +10,11 @@ from .serializers import UserSerializer
 
 # Create your views here.
 class UserViewSet(viewsets.ViewSet):
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def create(self, request):
         serializer = UserSerializer(data=request.data)
