ee672af35d436b2aced3d8636305dc8920fcf451
Victor-Drt <victorduarte778@gmail.com>
2026-01-14 00:30:40 -0400

    feat: remover comentario

--- users/serializers.py
+++ users/serializers.py
@@ -13,6 +13,6 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = User(**validated_data)
-        user.set_password(password)  # hash da senha
+        user.set_password(password)
         user.save()
         return user
